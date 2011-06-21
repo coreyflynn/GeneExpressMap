38158056f42a4fae24d14ffb3bc8abdf5d47a38b
Corey Flynn <flynn.corey@gmail.com>
2011-06-21 09:43:44 -0400

    updated MCT and MCTstack
    changes were made to make GEM compatible with the latest version of Krishnan Padmanabhan's MCT code

--- MCTstack.m
+++ MCTstack.m
@@ -1,14 +1,14 @@
 function [out,thresh]=MCTstack(in)
-% wrapper for Krishnan Padmanabhan's MCT algorithm to estend it to a stack of 
+% wrapper for Krishnan Padmanabhan's MCT algorithm to extend it to a stack of 
 % images.
 out=in*0;
 thresh=[];
 
 %for each image in the stack, apply MCT and store the computed threshold value
 for N=1:size(in,3)
-    m=MCT(in(:,:,N));
-    out(:,:,N)=m.image;
-    thresh=horzcat(thresh,m.tv);
+	[MCTimage, MCTrtv, MCTgtv, MCTbtv, MCTrcc, MCTgcc, MCTbcc, fbe] = MCT(in(:,:,N));
+    out(:,:,N)=MCTimage;
+    thresh=horzcat(thresh,MCTrtv);
 end
 
 %return the mean threshold value for the stack
